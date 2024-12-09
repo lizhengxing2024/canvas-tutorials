4e0d36388ee8e7634a24b4f4779c1b985fd8de09
李正兴 <lzx@dareway.com.cn>
2024-12-09 10:21:00 +0800

    1

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,8 +1,26 @@
 <template>
-  <div>
-    Hello
+  <div style="border: 1px solid blue">
+    <canvas ref="canvasDOM" width="400" height="300"></canvas>
   </div>
 </template>
 
 <script setup lang="ts">
+import { useTemplateRef, onMounted } from "vue";
+
+const canvasDOMRef = useTemplateRef("canvasDOM");
+
+onMounted(() => {
+  const ctx = canvasDOMRef.value!.getContext("2d")!;
+
+
+  ctx.shadowOffsetX = 6;
+  ctx.shadowOffsetY = 6;
+  ctx.shadowBlur = 6;
+  ctx.shadowColor = "rgba(0,0,255,.8)";
+
+  ctx.font = "80px Times New Roman";
+  ctx.strokeStyle = "green";
+  ctx.fillStyle = "blue";
+  ctx.strokeRect(100, 100, 100, 100);
+});
 </script>
