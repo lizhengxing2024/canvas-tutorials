164459804b5f8eb273b8d20a872e461848763b15
李正兴 <lzx@dareway.com.cn>
2024-12-16 14:08:06 +0800

    月经史

--- src/views/MenstrualHistory.vue
+++ src/views/MenstrualHistory.vue
@@ -12,27 +12,70 @@ import { useTemplateRef, onMounted } from "vue";
 
 const canvasDOMRef = useTemplateRef("canvasDOM");
 
+function measureText(
+  ctx: CanvasRenderingContext2D,
+  text: string
+): {
+  width: number;
+  height: number;
+} {
+  const measure = ctx.measureText(text);
+
+  return {
+    width: measure.width,
+    height: measure.actualBoundingBoxAscent + measure.actualBoundingBoxDescent,
+  };
+}
+
 onMounted(() => {
   const canvasDOM = canvasDOMRef.value;
   const ctx = canvasDOM?.getContext("2d")!;
 
+  const left = "16"; // 初潮年龄
+  const top = "10"; // 经期
+  const right = "40"; // 末次月经/绝经年龄
+  const bottom = "400"; // 周期
+
   ctx.beginPath();
-  ctx.font = "15px/10 宋体";
+  ctx.font = "15px 宋体";
+
+  const leftM = measureText(ctx, left);
+  const topM = measureText(ctx, top);
+  const rightM = measureText(ctx, right);
+  const bottomM = measureText(ctx, bottom);
+
+  ctx.textAlign = "left";
+  ctx.fillText(left, 0, leftM.height + topM.height);
+
   ctx.textAlign = "center";
+  ctx.fillText(
+    top,
+    leftM.width + Math.max(topM.width, bottomM.width) / 2,
+    topM.height
+  );
 
-  const measure = ctx.measureText("16");
-  const textHeight =
-    measure.actualBoundingBoxAscent + measure.actualBoundingBoxDescent;
-  const textWidth = measure.width;
+  ctx.textAlign = "left";
+  ctx.fillText(
+    right,
+    leftM.width + Math.max(topM.width, bottomM.width),
+    rightM.height + topM.height
+  );
 
-  ctx.fillText("16", textWidth / 2, textHeight * 2);
-  ctx.fillText("16", textWidth * 1.5, textHeight);
-  ctx.fillText("16", textWidth * 2.5, textHeight * 2);
-  ctx.fillText("16", textWidth * 1.5, textHeight * 3);
+  ctx.textAlign = "center";
+  ctx.fillText(
+    bottom,
+    leftM.width + Math.max(topM.width, bottomM.width) / 2,
+    topM.height + Math.max(leftM.height, rightM.height) + bottomM.height
+  );
 
-  ctx.moveTo(textWidth, textHeight * 1.5);
-  ctx.lineTo(textWidth * 2, textHeight * 1.5);
-
+  ctx.moveTo(
+    leftM.width,
+    topM.height + Math.max(leftM.height, rightM.height) / 2
+  );
+  ctx.lineTo(
+    leftM.width + Math.max(topM.width, bottomM.width),
+    topM.height + Math.max(leftM.height, rightM.height) / 2
+  );
   ctx.stroke();
 });
 </script>
